8fe6966512c535caac81571bd4ac6a434178f64e
Abhishek Vijayakumar <Abhishekwhizzer@gmail.com>
2023-04-20 22:05:47 -0400

    added initial config

---
+++ src/components/About.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full  grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+              About
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full  grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold ">
+            <p>Hi Im Abhishek vijayakumar. Please take a look around</p>
+          </div>
+          <div>
+            <p>
+              Front-end software developer with more than three years of
+              expertise in designing and creating commercial applications as
+              well as in enhancing software performance.A strong team player
+              with agile development expertisewho enjoys working both
+              individually and in aT eamenvironment with stakeholders,
+              designers.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
