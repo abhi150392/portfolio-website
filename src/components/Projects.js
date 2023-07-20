89c3d0573d5fd506f38c6d9d3e50f46eac3ebc4b
Abhishek Vijayakumar <abhishekwhizzer@gmail.com>
2023-07-19 23:51:26 -0500

    modidied projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,7 @@ import TaskTracker from "../assets/TaskTracker.jpg";
 import Movie from "../assets/Movie.jpg";
 import Form from "../assets/Form.jpg";
 import Quiz from "../assets/quiz.jpg";
+import Infinite from "../assets/infinite_scroll.png";
 
 const Projects = () => {
   return (
@@ -22,6 +23,36 @@ const Projects = () => {
         {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* grid-item */}
+          <div
+            style={{ backgroundImage: `url(${Movie})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Shopping Cart(React Context API)
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/abhi150392/Shopping-cart-contextapi-useReducer-"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold  text-lg">
+                    Code
+                  </button>
+                </a>
+                <a
+                  href="https://myshoppingcart123.netlify.app/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold  text-lg">
+                    Link
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
           <div
             style={{ backgroundImage: `url(${FoodOrder})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
@@ -98,31 +129,6 @@ const Projects = () => {
           </div>
 
           <div
-            style={{ backgroundImage: `url(${Movie})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Movie App
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://github.com/abhi150392/Movie-app"
-                  target="_blank"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold  text-lg">
-                    Code
-                  </button>
-                </a>
-                {/* <a href="">
-                  <button></button>
-                </a> */}
-              </div>
-            </div>
-          </div>
-
-          <div
             style={{ backgroundImage: `url(${Form})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
@@ -147,18 +153,43 @@ const Projects = () => {
             </div>
           </div>
 
-          <div
+          {/* <div
             style={{ backgroundImage: `url(${Quiz})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover effects */}
-            <div className="opacity-0 group-hover:opacity-100">
+          {/* <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
                 Quiz Functionality
               </span>
               <div className="pt-8 text-center">
                 <a
                   href="https://github.com/abhi150392/Quiz-functionality"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold  text-lg">
+                    Code
+                  </button>
+                </a>
+                {/* <a href="">
+                  <button></button>
+                </a> */}
+          {/* </div>
+            </div>
+          </div> */}
+
+          <div
+            style={{ backgroundImage: `url(${Infinite})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Infinite Scroll in React
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/abhi150392/Infinite-Scroll"
                   target="_blank"
                 >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold  text-lg">
