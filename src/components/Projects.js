265c43423f9e7c3239c36efac905d59115eb7bbd
Abhishek Vijayakumar <abhishekwhizzer@gmail.com>
2023-11-01 17:50:44 -0500

    added projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,7 +18,7 @@ const Projects = () => {
           <p className="text-4xl font-bold inline border-b-4 test-gray-300 border-pink-600">
             Projects
           </p>
-          <p className="py-6">// Check some of mh recent projects</p>
+          <p className="py-6">// Check some of my recent projects</p>
         </div>
         {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
